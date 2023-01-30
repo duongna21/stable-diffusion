93e8cb0a0e0d12308a9cbfee12be3bf9663b5cc9
anhduongng.1001 <anhduongng.1001@gmail.com>
2023-01-30 22:28:45 +0700

    add blip infer

--- blip_infer.py
+++ blip_infer.py
@@ -1,7 +1,9 @@
 import torch
 from lavis.models import load_model_and_preprocess
 from PIL import Image
-import os 
+import os
+from tqdm import tqdm
+
 # setup device to use
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
@@ -9,14 +11,18 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 # this also loads the associated image processors
 model, vis_processors, _ = load_model_and_preprocess(name="blip_caption", model_type="base_coco", is_eval=True, device=device)
 
+i = 0
 # load sample image
-for img in os.listdir("ffhq-512"):
-    raw_image = Image.open(f"ffhq-512/{img}").convert("RGB")
-    # preprocess the image
-    # vis_processors stores image transforms for "train" and "eval" (validation / testing / inference)
-    image = vis_processors["eval"](raw_image).unsqueeze(0).to(device)
-    # generate caption
-    pred = model.generate({"image": image})
-    print(pred)
-    # ['a large fountain spewing water into the air']
-    break
+with open('blip_captions.csv', 'w') as w:
+    for img in tqdm(os.listdir("ffhq-512")):
+        raw_image = Image.open(f"ffhq-512/{img}").convert("RGB")
+        # preprocess the image
+        # vis_processors stores image transforms for "train" and "eval" (validation / testing / inference)
+        image = vis_processors["eval"](raw_image).unsqueeze(0).to(device)
+        # generate caption
+        pred = model.generate({"image": image})
+        w.write(f"\n{img},{pred[0]}")
+        # ['a large fountain spewing water into the air']
+        i += 1
+        if i==5:
+            break
